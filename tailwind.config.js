6a36b57b9219af9c54ac4fb0f7b5feee6d016f7e
Daveena <kellyangel2330@gmail.com>
2024-11-24 02:28:56 +0100

    fix bug between nav and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       animation: {
         gradient: 'gradientBackground 10s ease infinite',
-        fadeIn: 'fadeIn 1.2s ease-out forwards', // Ajout de l'animation fadeIn
+        fadeIn: 'fadeIn 1.2s ease-out forwards',
       },
       keyframes: {
         gradientBackground: {
@@ -15,15 +16,9 @@ module.exports = {
           '50%': { 'background-position': '100% 50%' },
           '100%': { 'background-position': '0% 50%' },
         },
-        fadeIn: { // Définition de l'animation fadeIn
-          '0%': {
-            opacity: 0,
-            transform: 'translateY(10px)',
-          },
-          '100%': {
-            opacity: 1,
-            transform: 'translateY(0)',
-          },
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(10px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
         },
       },
     },
