9acf1982b84acca7e0680235a7018c9fdbca0a90
Daveena <kellyangel2330@gmail.com>
2024-11-25 11:01:56 +0100

    update the next.config folder and package.json folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Option recommandée
+  images: {
+    domains: [], // Ajoutez ici vos domaines pour les images externes si nécessaire
+  },
 };
 
 export default nextConfig;
